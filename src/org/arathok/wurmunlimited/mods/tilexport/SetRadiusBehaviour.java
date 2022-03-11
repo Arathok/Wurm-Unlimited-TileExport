759ae9bdc3305dca24c9790e1a113157450bb3ed
Arathok <36503011+Arathok@users.noreply.github.com>
2022-03-11 10:13:40 +0100

    update 1.1

--- src/org/arathok/wurmunlimited/mods/tilexport/SetRadiusBehaviour.java
+++ src/org/arathok/wurmunlimited/mods/tilexport/SetRadiusBehaviour.java
@@ -21,12 +21,12 @@ import java.util.List;
         public SetRadiusBehaviour() {
             menu = new LinkedList<>();
             menu.add(new ActionEntry((short) -6, "Set Survey Radius to:", ""));
-            menu.add(new SetRadiusAction(Config.value1, "5 Tiles").actionEntry);
-            menu.add(new SetRadiusAction(Config.value2, "10 Tiles").actionEntry);
-            menu.add(new SetRadiusAction(Config.value3, "15 Tiles").actionEntry);
-            menu.add(new SetRadiusAction(Config.value4, "20 Tiles").actionEntry);
-            menu.add(new SetRadiusAction(Config.value5, "25 Tiles").actionEntry);
-            menu.add(new SetRadiusAction(Config.value6, "50 Tiles").actionEntry);
+            menu.add(new SetRadiusAction(Config.value1, Config.value1+" Tiles").actionEntry);
+            menu.add(new SetRadiusAction(Config.value2, Config.value2+" Tiles").actionEntry);
+            menu.add(new SetRadiusAction(Config.value3, Config.value3+" Tiles").actionEntry);
+            menu.add(new SetRadiusAction(Config.value4, Config.value4+" Tiles").actionEntry);
+            menu.add(new SetRadiusAction(Config.value5, Config.value5+" Tiles").actionEntry);
+            menu.add(new SetRadiusAction(Config.value6, Config.value6+" Tiles").actionEntry);
 
         }
 
